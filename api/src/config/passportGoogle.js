1622e8c776f8aeaefdcf213a4f9b69331e106f24
Juan José Rodriguez <91842391+Zoranspiegel@users.noreply.github.com>
2023-02-09 00:08:11 -0500

    Update passportGoogle.js

--- api/src/config/passportGoogle.js
+++ api/src/config/passportGoogle.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${FRONT_URL}/api/v1/auth/google/callback`,
+      callbackURL: `${process.env.FRONT_URL}/api/v1/auth/google/callback`,
       passReqToCallback: true,
     },
     async (req, accessToken, refreshToken, profile, cb) => {
