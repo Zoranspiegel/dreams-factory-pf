fe3d8fe475efb27e448d915c52e2aaacba54f605
Juan José Rodriguez <91842391+Zoranspiegel@users.noreply.github.com>
2023-02-09 08:33:36 -0500

    Update loginGoogle.routes.js

--- api/src/routes/loginGoogle.routes.js
+++ api/src/routes/loginGoogle.routes.js
@@ -5,7 +5,7 @@ const User = require("../models/User");
 const router = Router();
 
 const home = `${process.env.FRONT_URL}`;
-const successLoginUrl = `${process.env.FRONT_URL}`;
+const successLoginUrl = `${process.env.FRONT_URL}/login/success`;
 const errorLoginUrl = `${process.env.FRONT_URL}/login/error`;
 
 
